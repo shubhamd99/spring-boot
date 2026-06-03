ef6f01f0c3c0c297530c1641f634a0fb3217017c
shubhamd99 <shubhamdhage930@gmail.com>
2026-06-03 22:41:06 +0530

    demo spring without boot

---
+++ DemoSpringWithoutBoot/src/main/java/org/shubham/Dev.java
@@ -0,0 +1,12 @@
+package org.shubham;
+
+public class Dev {
+    // constructor
+    public Dev() {
+        System.out.println("Dev constructor");
+    }
+
+    public void build() {
+        System.out.println("working on Awesome project!!");
+    }
+}
---
+++ DemoSpringWithoutBoot/src/main/java/org/shubham/Laptop.java
@@ -0,0 +1,12 @@
+package org.shubham;
+
+public class Laptop {
+    // constructor
+    public Laptop() {
+        System.out.println("Laptop constructor");
+    }
+
+    public void build() {
+        System.out.println("working on Awesome !!");
+    }
+}

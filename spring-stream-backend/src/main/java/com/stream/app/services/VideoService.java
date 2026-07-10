5bdcbd1d020941e3380add2773730a3ef249215c
shubhamd99 <shubhamdhage930@gmail.com>
2026-07-10 14:42:31 +0530

    video stream backend java service

---
+++ spring-stream-backend/src/main/java/com/stream/app/services/VideoService.java
@@ -0,0 +1,22 @@
+package com.stream.app.services;
+
+import com.stream.app.entities.Video;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface VideoService {
+    // save video
+    Video save(Video video, MultipartFile file);
+
+    // get video by id
+    Video get(String id);
+
+    // get video by title
+    Video getByTitle(String title);
+
+    // get all videos
+    List<Video> getAll();
+
+    String processVideo(String videoId);
+}
